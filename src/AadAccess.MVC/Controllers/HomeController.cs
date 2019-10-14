ae3a801128f8948c370ef649d745f91608eaf314
Michał Chęciński <michal.checinski@outlook.com>
2019-10-14 15:55:24 +0200

    Add service for keyvault

--- src/AadAccess.MVC/Controllers/HomeController.cs
+++ src/AadAccess.MVC/Controllers/HomeController.cs
@@ -9,17 +9,21 @@
     {
         private readonly FunctionsValueService _functionsValueService;
         private readonly ApiValueService _apiValueService;
+        private readonly KeyVaultService _keyVaultService;
 
         public HomeController()
         {
             _functionsValueService = new FunctionsValueService();
             _apiValueService = new ApiValueService();
+            _keyVaultService = new KeyVaultService();
         }
         
         public IActionResult Index()
         {
             ViewData["FunctionsValue"] = _functionsValueService.GetValue("ValueFunction");
             ViewData["ApiValue"] = _apiValueService.GetValue("Value");
+            ViewData["KeyVaultValue"] = _keyVaultService.GetSecretValue;
+
             return View();
         }
 
---
+++ src/AadAccess.MVC/Services/KeyVaultService.cs
@@ -0,0 +1,30 @@
+﻿namespace AadAccess.MVC.Services
+{
+    using System.Configuration;
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.Azure.Services.AppAuthentication;
+
+    public class KeyVaultService
+    {
+        private readonly string _keyVaultUrl;
+        private readonly KeyVaultClient _keyVaultClient;
+
+        public KeyVaultService()
+        {
+            _keyVaultUrl = ConfigurationManager.AppSettings["KeyVaultUrl"];
+
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            _keyVaultClient =
+                new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+        }
+
+        public string GetSecretValue => GetSecret("Secret");
+
+        private string GetSecret(string secretName)
+        {
+            var sec = _keyVaultClient.GetSecretAsync(_keyVaultUrl, secretName);
+            return sec.Result.Value;
+        }
+    }
+}
